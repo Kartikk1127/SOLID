1d2c27793e3e390cd3718fa998f2ee63de7b254a
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-03-31 06:19:33 +0530

    single responsibility followed

---
+++ src/main/java/com/kartikey/InvoicePrinter.java
@@ -0,0 +1,11 @@
+public class InvoicePrinter {
+    Invoice invoice;
+
+    public InvoicePrinter(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+    public void print(){
+        //logic to print the invoice
+    }
+}
